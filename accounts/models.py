550c91f20af7f24ce8b3066690504d10a4f181e4
kimhangyu <khk201@naver.com>
2024-09-27 15:21:59 +0900

    feat. reports 모델 정의 및 앱 간 순환 참조문제 해결
    -accounts/models.py 수정
    -reports/models.py 작성
    -MainReport, ChartReport, NewsReport, ReportWeights 모델 정의
    -모델관계개선
    -comments 모델 생성 (Redis활용할 예정)

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# 사용자 모델
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     nickname = models.CharField(max_length=150, unique=True)
     username = models.CharField(max_length=150)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
-    comments = models.TextField(blank=True)
-    likes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)   # 사용자가 받은 총 좋아요 수
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nickname', 'username']
 
     def __str__(self):
         return self.email
+
+# 코멘트 모델
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    report = models.ForeignKey('reports.MainReport', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.nickname} on {self.report.title}"
