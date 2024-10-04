2456f6ec1e95f8f347b5b7fe40ff806e751fc331
kimhangyu <khk201@naver.com>
2024-10-04 16:25:22 +0900

    edit chromedriver

--- reports/services/chart_service.py
+++ reports/services/chart_service.py
@@ -31,17 +31,38 @@ class ChartCapture:
     def __init__(self):
         self.driver = None
 
-    def create_driver(self):
+    # 로컬 개발용
+    # def create_driver(self):
+    #     logger.info("ChromeDriver 설정 중...")
+    #     try:
+    #         chrome_options = Options()
+    #         chrome_options.add_argument("--headless")
+    #         chrome_options.add_argument("--start-maximized")
+    #         chrome_options.add_argument("--no-sandbox")
+    #         chrome_options.add_argument("--disable-dev-shm-usage")
+    #         service = Service(ChromeDriverManager().install())
+    #         self.driver = webdriver.Chrome(service=service, options=chrome_options)
+    #         return self.driver
+    #     except Exception as e:
+    #         logger.error(f"ChromeDriver 생성 중 오류 발생: {e}")
+    #         raise
+
+    # EC2 서버용
+    def create_driver():
         logger.info("ChromeDriver 설정 중...")
         try:
             chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--start-maximized")
+            chrome_options.add_argument("--headless")  # 헤드리스 모드 사용
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
-            service = Service(ChromeDriverManager().install())
-            self.driver = webdriver.Chrome(service=service, options=chrome_options)
-            return self.driver
+            chrome_options.add_argument("--disable-gpu")
+
+            service = Service('/usr/bin/chromedriver')  # Specify the path to the ChromeDriver executable
+
+            # Initialize the WebDriver with the specified options
+            driver = webdriver.Chrome(service=service, options=chrome_options)
+
+            return driver
         except Exception as e:
             logger.error(f"ChromeDriver 생성 중 오류 발생: {e}")
             raise
