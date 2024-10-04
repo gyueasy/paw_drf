cf2ea9898f70b4080bbc2e7a510c843c6d760221
kimhangyu <khk201@naver.com>
2024-10-04 22:03:50 +0900

    chart path 변경

--- reports/services/chart_service.py
+++ reports/services/chart_service.py
@@ -128,15 +128,15 @@ class ChartCapture:
     def _perform_chart_actions(self):
         try:
             self._click_element_by_xpath(
-                "//cq-menu[@class='ciq-menu ciq-period']",
+                "//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[1]/span/cq-clickable",
                 "시간 메뉴"
             )
             self._click_element_by_xpath(
-                "//cq-item[./translate[text()='1시간']]",
+                "//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[1]/cq-menu-dropdown/cq-item[8]/translate",
                 "1시간 옵션"
             )
             self._click_element_by_xpath(
-                "//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[2]/span",
+                "//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[2]/span/translate",
                 "차트설정"
             )
             dark_theme_xpath = "//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[2]/cq-menu-dropdown/cq-themes/cq-themes-builtin/cq-item[2]"
@@ -147,10 +147,10 @@ class ChartCapture:
                 ("//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[3]/cq-menu-dropdown/cq-scroll/cq-studies/cq-studies-content/cq-item[81]", "RSI"),
                 ("//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[3]/cq-menu-dropdown/cq-scroll/cq-studies/cq-studies-content/cq-item[53]", "MACD")
             ]
-
+            
             for indicator_xpath, indicator_name in indicators:
                 self._click_element_by_xpath(
-                    "/html/body/div[1]/div[3]/div[3]/span/div/div/div[1]/div/div/cq-menu[3]",
+                    "//*[@id='fullChartiq']/div/div/div[1]/div/div/cq-menu[3]/span/translate",
                     "지표 메뉴"
                 )
                 self._click_indicator_option(indicator_xpath, indicator_name)
@@ -178,53 +178,6 @@ class ChartCapture:
             logger.error(f"스크린샷 캡처 및 저장 중 오류 발생: {e}")
             return None
         
-    # #ec2 서버용
-    # def _capture_and_save_screenshot(self):
-    #     try:
-    #         # 페이지가 완전히 로드될 때까지 대기
-    #         WebDriverWait(self.driver, 20).until(
-    #             EC.presence_of_element_located((By.XPATH, '//*[@id="fullChartiq"]/div/div'))
-    #         )
-
-    #         # 차트 요소 찾기
-    #         chart_element = self.driver.find_element(By.XPATH, '//*[@id="fullChartiq"]/div/div')
-
-    #         # 차트 요소의 크기 가져오기
-    #         size = chart_element.size
-            
-    #         # 창 크기를 차트 크기에 맞게 조정 (높이에 여유를 두기 위해 100px 추가)
-    #         self.driver.set_window_size(size['width'], size['height'] + 100)
-
-    #         # 차트 요소의 위치 정보 가져오기
-    #         location = chart_element.location
-
-    #         # 전체 스크린샷 찍기
-    #         png = self.driver.get_screenshot_as_png()
-
-    #         # 이미지 처리
-    #         im = Image.open(io.BytesIO(png))
-    #         left = location['x']
-    #         top = location['y']
-    #         right = left + size['width']
-    #         bottom = top + size['height']
-
-    #         # 차트 부분만 크롭
-    #         im = im.crop((left, top, right, bottom))
-
-    #         # 이미지 저장
-    #         current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    #         filename = f"chart_screenshot_{current_time}.png"
-    #         save_dir = os.path.join(settings.MEDIA_ROOT, 'capture_chart')
-    #         os.makedirs(save_dir, exist_ok=True)
-    #         file_path = os.path.join(save_dir, filename)
-    #         im.save(file_path)
-
-    #         logger.info(f"스크린샷이 저장되었습니다: {file_path}")
-    #         image_url = f"{settings.MEDIA_URL}capture_chart/{filename}"
-    #         return image_url
-    #     except Exception as e:
-    #         logger.error(f"스크린샷 캡처 및 저장 중 오류 발생: {e}")
-    #         return None
 
     def _save_current_price(self):
         current_price = get_current_price()
