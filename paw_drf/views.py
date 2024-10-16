609c590ac1d16f15458d04189c62eba3df1908ff
kimhangyu <khk201@naver.com>
2024-10-16 21:38:51 +0900

    test time

--- paw_drf/views.py
+++ paw_drf/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core.cache import cache
+from django.utils import timezone
 
 def health_check(request):
     return HttpResponse("OK")
@@ -10,7 +11,9 @@ def test_redis_cache(request):
     cached_value = cache.get(cache_key)
     
     if cached_value is None:
-        cached_value = "This is a test value"
+        current_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
+        cached_value = f"This is a test value set at {current_time}"
         cache.set(cache_key, cached_value, timeout=300)  # 5분 동안 캐시
-    
-    return JsonResponse({"cached_value": cached_value})
+        return JsonResponse({"cached_value": cached_value, "status": "newly set"})
+    else:
+        return JsonResponse({"cached_value": cached_value, "status": "from cache"})
