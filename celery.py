ba506783260f50e1ba90f25bbb1139aab0e7332f
kimhangyu <khk201@naver.com>
2024-10-17 15:21:36 +0900

    error fix-1

--- celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paw_drf.settings')
-
-app = Celery('paw_drf')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
