208cc82b9060c82a5b7abec57d5b8b978fb8162c
kimhangyu <khk201@naver.com>
2024-10-17 14:43:16 +0900

    settings, celery install

--- paw_drf/settings.py
+++ paw_drf/settings.py
@@ -104,6 +104,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django_json_widget',
     'django_extensions',
+    'django_celery_beat',
     # Third-party apps
     'allauth',
     'allauth.account',
@@ -189,6 +190,13 @@ CACHES = {
         }
     }
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'  # Redis를 브로커로 사용
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Seoul'
 
 CELERY_BEAT_SCHEDULE = {
     'generate-reports-every-day': {
